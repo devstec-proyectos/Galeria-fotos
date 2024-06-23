e62d2f492a11e29c637aa7f66c16f05c864ea810
elisay1 <castanedaelisayes@gmail.com>
2024-06-22 20:45:18 -0500

    Usando media query

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const thumbnails = document.querySelectorAll('.thumbnail img');
-    const mainImage = document.querySelector('main-image img');
+const btnLeft = document.querySelector(".btn-left"),
+  btnRight = document.querySelector(".btn-right"),
+  slider = document.querySelector("#slider"),
+  sliderSection = document.querySelectorAll(".thumbnail");
 
-    thumbnails.forEach(thumbnail=>{
-        thumbnail.addEventListener('click', function(){
-            mainImage.src = thumbnail.src;
-        })
-    })
-})
+btnLeft.addEventListener("click", (e) => moveToLeft());
+btnRight.addEventListener("click", (e) => moveToRight());
+
+setInterval(() => {
+  moveToRight();
+}, 3000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+    
+}   
